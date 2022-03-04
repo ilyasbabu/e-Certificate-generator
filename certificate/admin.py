0470d3b46ad2b179da2afb1d0c5d174c82da1262
ilyasbabu <ilyasbabuvp@gmail.com>
2022-03-04 11:57:55 +0530

    models registered in admin

--- certificate/admin.py
+++ certificate/admin.py
@@ -1,1 +1,5 @@
 from .models import *
+from django.contrib import admin
+
+admin.site.register(Event)
+admin.site.register(Participant)
--- user/admin.py
+++ user/admin.py
@@ -1,2 +1,5 @@
 from django.contrib import admin
 
+from user.models import UserOTP
+admin.site.register(UserOTP)
+
